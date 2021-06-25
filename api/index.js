e87164c9220081fbe06ce527e8924cb88132c3bb
fahrizaasril <84886351+fahrizaasril@users.noreply.github.com>
2021-06-25 23:31:17 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,24 +16,60 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        click  /predict to know about x y and z`
     );   
 });
 
-bot.onText(/\/menu/, (msg) => { 
-    console.log(msg)
+
+
+state = 0;
+bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `this is your main menu`
+        `input nilai x|y|z example 4|3|2`
     );   
+    state = 1;
 });
 
+bot.on('message', (msg) => {
+    if(state == 1){
+        s = msg.text.split("|");
+        x = s[0]
+        y = s[1]
+        z = s[2]
+        model.predict(
+            [
+                parseFloat(s[0]), // string to float
+                parseFloat(s[1]),
+                parseFloat(s[2])
+            ]
+        ).then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai x yang diprediksi adalah ${jres[0]} `
+            );   
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai y yang diprediksi adalah ${jres[1]} `
+            );   
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai z yang diprediksi adalah ${jres[2]} `
+         );   
+})
+    }else{
+        state = 0
+    }
+})
+
+
 // routers
-r.get('/prediction/:i/:r', function(req, res, next) {    
+r.get('/prediction/:x/:y/:z', function(req, res, next) {    
     model.predict(
         [
-            parseFloat(req.params.i), // string to float
-            parseFloat(req.params.r)
+            parseFloat(req.params.x), // string to float
+            parseFloat(req.params.y),
+            parseFloat(req.params.z)
         ]
     ).then((jres)=>{
         res.json(jres);
